189e38efd23f9e01e469d0eec658737c459a8f06
松尾清美 <>
2024-05-01 17:09:29 +0900

    レイアウト、投稿フォーム機能について

--- resources/views/layouts/logout.blade.php
+++ resources/views/layouts/logout.blade.php
@@ -6,8 +6,13 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="description" content="ページの内容を表す文章" />
   <title></title>
+  <!--Bootstrap（css）を反映させる記述-->
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
   <link rel="stylesheet" href="{{ asset('css/reset.css') }} ">
   <link rel="stylesheet" href="{{ asset('css/logout.css') }} ">
+
   <!--スマホ,タブレット対応-->
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <!--サイトのアイコン指定-->
@@ -18,14 +23,22 @@
   <!--iphoneのアプリアイコン指定-->
   <link rel="apple-touch-icon-precomposed" href="画像のURL" />
   <!--OGPタグ/twitterカード-->
+
+  <!--Bootstrap（js）を反映させる記述-->
+  <script src="{{ asset('js/app.js') }}"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </head>
 <body>
-  <header>
-    <h1><img src="images/atlas.png"></h1>
-    <p>Social Network Service</p>
-  </header>
-  <div id="container">
-    @yield('content')
+  <div class="Atlas">
+    <header>
+      <h1>
+        <img class="logo" src="images/atlas.png">
+      </h1>
+      <p class="sns">Social Network Service</p>
+    </header>
+    <div id="container">
+      @yield('content')
+    </div>
   </div>
   <script src="JavaScriptファイルのURL"></script>
   <script src="JavaScriptファイルのURL"></script>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,37 @@
 @extends('layouts.login')
 
 @section('content')
-<h2>機能を実装していきましょう。</h2>
+<div>
+  <ul>
+    <li>
+      <div class="my-post">
+        <h2><img src="images/icon1.png"></h2>
+        {!! Form::open(['url' => '/post/create']) !!}
+        <div>
+          {{ Form::textarea('post', null,['required', 'class' => 'form-control', 'placeholder' => '投稿内容を入力してください。'])}}
+        </div>
+        <button type="submit"><img class="post-send" src="images/post.png"></button>
+        {!! Form::close() !!}
+      </div>
+    </li>
+    <li>
+      @foreach ($posts as $post)
+      <ul>
+        <div class="post-block">
+         <li>
+            <figure><img src="https://placehold.jp/50x50.png" alt="Aさん"></figure>
+            <div class="post-content">
+              <div>
+                <div class="post-name">{{ $post ->user ->username }}さん</div>
+                <div>{{ $post -> created_at }}</div>
+              </div>
+            <div>{{ $post -> post }}</div>
+          </li>
+        </div>
+      </ul>
+      @endforeach
+    </li>
+  </ul>
+</div>
 
 @endsection
