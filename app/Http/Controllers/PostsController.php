16ff2d2fa3700d29a7bd4a32f58b59ac305eb2f7
松尾清美 <>
2024-05-01 17:06:44 +0900

    投稿フォームについて

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+use App\Post;
 
 class PostsController extends Controller
 {
-    //
     public function index(){
-        return view('posts.index');
+        $posts = Post::get();
+        return view('posts.index',['posts'=>$posts]);
+    }
+
+    //投稿の処理
+    public function postCreate(Request $request){
+        $post = $request->post;
+
+        Post::create([               //新しい投稿に必要なカラムを入れる
+            'post' => $post,         //postの中に変数$post
+            'user_id' => Auth::id()  //user_idの中に現在認証しているユーザーのIDを取得
+        ]);
+        return back();
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'user_id', 'post',
+    ];
+
+    //「１対多」の「1」側 → メソッド名は単数形でbelongsToを使う
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
 }
