6a11e6ef14ea97528f6fd7b9889385d092768dd2
松尾清美 <>
2024-06-18 19:20:01 +0900

    検索について

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Follow;
 
 class UsersController extends Controller
 {
@@ -10,7 +12,17 @@ class UsersController extends Controller
     public function profile(){
         return view('users.profile');
     }
-    public function search(){
-        return view('users.search');
+    public function search(Request $request){
+        // 1つ目の処理
+        $keyword = $request->input('keyword');
+        // 2つ目の処理
+        if(!empty($keyword)){
+            $user = User::where('username','like', '%'.$keyword.'%')->get();
+            //dd($user);
+            }else{
+             $user = User::all();
+        }
+        // 3つ目の処理
+        return view('users.search',['users'=>$user,'keyword'=>$keyword]);
     }
 }
--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -1,7 +1,23 @@
 @extends('layouts.login')
 
 @section('content')
+<div class="search">
+  <form action="#" id="form">
+      @csrf
+      <input type="text" name="keyword" placeholder="ユーザー名">
+      <button type="submit"><img class="search-btn" src="images/search.png"></button>
+    </form>
+    @if($keyword)
+      {{ "検索ワード：" .$keyword }}
+    @endif
+    @foreach ($users as $user)
+    <ul>
+        <li><img src="{{ asset('images/'.Auth::user()->images) }}" ></li>
+        <li>{{ $user->username }}</li>
+    </ul>
+    @endforeach
 
+</div>
 
 
 @endsection
