141af88b1bab55f9ecf3ecc41ea79eec1ac84340
松尾清美 <>
2024-08-07 20:22:06 +0900

    no message

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -4,29 +4,39 @@
 {!! Form::open(['url' => '/profile']) !!}
 
 <div class="edit-profile">
-  <img src="{{ asset('images/'.Auth::user()->images) }}" >
+  <img class="icon" src="{{ asset('images/'.Auth::user()->images) }}" >
+  <div>
+  {{ Form::label('ユーザー名') }}
+  {{ Form::text('username',Auth::user()->username,['class' => 'input']) }}
+  </div>
 
-{{ Form::label('ユーザー名') }}
-{{ Form::text('username',null,['class' => 'input']) }}
+  <div>
+  {{ Form::label('メールアドレス') }}
+  {{ Form::text('mail',Auth::user()->mail,['class' => 'input']) }}
+  </div>
 
-{{ Form::label('メールアドレス') }}
-{{ Form::text('mail',null,['class' => 'input']) }}
+  <div>
+  {{ Form::label('パスワード') }}
+  {{ Form::password('password',null,['class' => 'input']) }}
+  </div>
 
-{{ Form::label('パスワード') }}
-{{ Form::text('password',null,['class' => 'input']) }}
+  <div>
+  {{ Form::label('パスワード確認') }}
+  {{ Form::text('password_confirmation',null,['class' => 'input']) }}
+  </div>
 
-{{ Form::label('パスワード確認') }}
-{{ Form::text('password_confirmation',null,['class' => 'input']) }}
+  <div>
+  {{ Form::label('自己紹介') }}
+  {{ Form::text('self-introduction',Auth::user()->bio,['class' => 'input']) }}
+  </div>
 
-{{ Form::label('自己紹介') }}
-{{ Form::text('self-introduction',null,['class' => 'input']) }}
+  <div>
+  {{ Form::label('アイコン画像') }}
+  {{ Form::file('icon-image',null,['class' => 'input']) }}
+  </div>
+  {{ Form::submit('更新') }}
 
-{{ Form::label('アイコン画像') }}
-{{ Form::text('icon-image',null,['class' => 'input']) }}
-
-{{ Form::submit('更新') }}
-
-{!! Form::close() !!}
+  {!! Form::close() !!}
 
 </div>
 
