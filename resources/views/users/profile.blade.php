dec145b1ce0a4b097788bf64af276a72d9f6c53c
松尾清美 <>
2024-08-16 21:07:45 +0900

    バリデーション、アイコンの呼び出しファイル

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -1,10 +1,23 @@
 @extends('layouts.login')
 
 @section('content')
-{!! Form::open(['url' => '/profile']) !!}
+
+@if($errors->any())
+<div class="alert alert-danger">
+  <ul>
+    @foreach($errors->all() as $error)
+    <li>{{ $error }}</li>
+    @endforeach
+  </ul>
+</div>
+@endif
+
+{!! Form::open(['url' => '/profile/update','enctype' => 'multipart/form-data']) !!}
+{{ Form::hidden('id',Auth::id()) }}
+
 
 <div class="edit-profile">
-  <img class="icon" src="{{ asset('images/'.Auth::user()->images) }}" >
+  <img class="icon" src="{{ asset('/storage/images/'.Auth::user()->images) }}" >
   <div>
   {{ Form::label('ユーザー名') }}
   {{ Form::text('username',Auth::user()->username,['class' => 'input']) }}
@@ -17,24 +30,26 @@
 
   <div>
   {{ Form::label('パスワード') }}
-  {{ Form::password('password',null,['class' => 'input']) }}
+  {{ Form::password('password',['class' => 'input']) }}
   </div>
 
   <div>
   {{ Form::label('パスワード確認') }}
-  {{ Form::text('password_confirmation',null,['class' => 'input']) }}
+  {{ Form::password('password_confirmation',['class' => 'input']) }}
   </div>
 
   <div>
   {{ Form::label('自己紹介') }}
-  {{ Form::text('self-introduction',Auth::user()->bio,['class' => 'input']) }}
+  {{ Form::text('bio',Auth::user()->bio,['class' => 'input']) }}
   </div>
 
   <div>
   {{ Form::label('アイコン画像') }}
-  {{ Form::file('icon-image',null,['class' => 'input']) }}
+  {{ Form::file('icon_image',null,['class' => 'input']) }}
   </div>
-  {{ Form::submit('更新') }}
+
+  <button type="submit" class="btn btn-danger">更新</button>
+  </a>
 
   {!! Form::close() !!}
 
